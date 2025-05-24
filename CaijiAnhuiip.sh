a7f5201358cd0b94bbc067664b3fc46e1fe96bfe
yktcfy <zjyktcfy@qq.com>
2025-05-24 11:57:31 +0800

    Update and rename zubo2.sh to CaijiAnhuiip.sh

--- CaijiAnhuiip.sh
+++ CaijiAnhuiip.sh
@@ -3,7 +3,7 @@ if [ $# -eq 0 ]; then
   echo "开始测试······"
   echo "在5秒内输入1~4可选择城市"
   echo "请选择城市："
-  echo "1. 安徽电信"
+  echo "1. 安徽（Anhui_dianxin）"
   read -t 5 -p "超时未输入,将按默认设置测试" city_choice
 
   if [ -z "$city_choice" ]; then
@@ -17,7 +17,7 @@ fi
 # 设置城市和相应的stream
 case $city_choice in
     1)
-        city="安徽电信"
+        city="Anhui_dianxin"
         stream="rtp/238.1.79.42:4448"
         ;;
     0)
